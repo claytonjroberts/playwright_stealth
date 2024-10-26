59a7e3b79ac3b7e59de81b84cb544204416563aa
Mattwmaster58 <mattwmaster58@gmail.com>
2024-10-26 16:29:17 -0400

    fix: chrome.hairline.js

--- playwright_stealth/js/evasions/chrome.hairline.js
+++ playwright_stealth/js/evasions/chrome.hairline.js
@@ -1,5 +1,7 @@
 log("loading chrome.hairline.js");
 // inspired by: https://intoli.com/blog/making-chrome-headless-undetectable/
+const elementDescriptor = Object.getOwnPropertyDescriptor(HTMLElement.prototype, "offsetHeight");
+
 utils.replaceProperty(HTMLDivElement.prototype, "offsetHeight", {
   get: function () {
     // hmmm not sure about this
